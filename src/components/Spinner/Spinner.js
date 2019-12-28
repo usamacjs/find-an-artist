6dee880fba2d55ad032732022c508fd2cecf1353
usamacjs <usamacjs@hotmail.com>
2019-12-29 00:54:01 +0300

    spinner bug fixed after testing on staging

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Spinner.scss";
 
 const Spinner = () => {
   return (
-    <>
+    <React.Fragment>
       <div className="spinneroverlay">
         <div className="spinneroverlay__container" />
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
