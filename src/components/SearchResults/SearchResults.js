1fa662df3f5d239e4a8642356f4d7ef3c435c2f9
usamacjs <usamacjs@hotmail.com>
2019-12-29 23:59:19 +0300

    added useEffect for search results to render data from local storage and search title changed so it makes better sense

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ArtistInfo from "../Cards/ArtistInfo/ArtistInfo";
 import Container from "../Container/Container";
 import "./SearchResults.scss";
 
 const SearchResults = ({ artistData, artistName, setShowEvents }) => {
+  useEffect(() => {}, [artistData]);
   const lastArtistSearched = localStorage.getItem("lastArtistSearched");
   if (artistData && !artistData.error) {
     const { name } = artistData;
     return (
       <Container>
         <p className="search-title">
-          <strong>1</strong> Result found for "<strong>{name}</strong>"
+          <strong>1</strong> Result found for your last search "
+          <strong>{name}</strong>"
         </p>
         <div className="search-results">
           <ArtistInfo
@@ -22,11 +24,11 @@ const SearchResults = ({ artistData, artistName, setShowEvents }) => {
       </Container>
     );
   }
-  if (artistData && (artistData === "" || artistData.error)) {
+  if (artistData && (artistData == "" || artistData.error)) {
     return (
       <Container>
         <p className="search-title">
-          <strong>0</strong> Result found for "
+          <strong>0</strong> Result found for your last search "
           <strong>{lastArtistSearched && lastArtistSearched}</strong>"
         </p>
       </Container>
